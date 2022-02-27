6f28b87c058974e04a1a9d64707f3e07b0b06586
Yuewei Wang <yuewei.wang@mail.utoronto.ca>
2022-02-27 11:51:59 -0500

    place in folder

---
+++ BookMainPage/BookMainPage.js
@@ -0,0 +1,107 @@
+// global variables
+var BooksNum = 0; 
+var BooksList = [] 
+
+class Book {
+	constructor(name, author, year, coverURL, description) {
+		this.name = name;
+		this.author = author;
+		this.year = year;
+		this.coverURL = coverURL; 
+        this.description = description;
+        this.postCollection = [] // collection of post ids associated with the book
+		this.bookID = BooksNum;
+		BooksNum++;
+	}
+}
+
+const bookTable = document.querySelector('#bookTable')
+
+// Load default book data
+BooksList.push(new Book('Solaris', 'Stanisław Herman Lem', 1970, 
+'https://upload.wikimedia.org/wikipedia/en/d/d1/SolarisNovel.jpg', 
+'It follows a crew of scientists on a research station as they attempt to understand an extraterrestrial intelligence, which takes the form of a vast ocean on the titular alien planet.'))
+
+BooksList.push(new Book('Tres Tristes Tigres', 'Guillermo Cabrera Infante', 1971,
+'https://upload.wikimedia.org/wikipedia/en/0/0f/Tres_tristes_tigres_%28Guillermo_Cabrera_Infante%29.png',
+'It is a highly experimental, Joycean novel, playful and rich in literary allusions.'))
+
+// Display all books in the book main page
+window.onload = function displayAllBooks() {
+    const bookTable = document.querySelector('#bookTable')
+	for(let i = 0; i < BooksNum; i++) {
+		const div = document.createElement('div')
+        div.className = 'book'
+        
+        // book name 
+        const p1 = document.createElement('p')
+        const strong1 = document.createElement('strong')
+        const name = document.createTextNode("Book Name: ")
+        strong1.appendChild(name)
+        p1.appendChild(strong1)
+
+        const span1 = document.createElement('span')
+        span1.className="bookTitle"
+        const a = document.createElement('a')
+        a.className = "linkColor"
+        a.href = ""
+        const nameContent = document.createTextNode(BooksList[i].name)
+        a.appendChild(nameContent)
+        span1.appendChild(a)
+        p1.appendChild(span1)
+        div.appendChild(p1)
+
+        // cover img
+        const img = document.createElement('img')
+        img.className = "cover"
+        img.src = BooksList[i].coverURL
+        img.alt = "cover"
+        div.appendChild(img)
+
+        // description
+        const p2 = document.createElement('p')
+        p2.className = "descriptionsBox"
+        const strong2 = document.createElement('strong')
+        const descri = document.createTextNode("Descriptions: ")
+        strong2.appendChild(descri)
+        p2.appendChild(strong2)
+
+        const span2 = document.createElement('span')
+        const descriContent = document.createTextNode(BooksList[i].description)
+        span2.appendChild(descriContent)
+        p2.appendChild(span2)
+        div.appendChild(p2)
+
+        // info table
+        const table = document.createElement('table')
+        table.className = "bookinfo"
+        const tbody = document.createElement('tbody')
+        const tr1 = document.createElement('tr')
+	    const ID = document.createElement('th')
+        ID.innerText = 'Book ID'
+        const author = document.createElement('th')
+	    author.innerText = 'Author'
+        const year = document.createElement('th')
+	    year.innerText = 'Publish year'
+        tr1.appendChild(ID)
+        tr1.appendChild(author)
+        tr1.appendChild(year)
+        tbody.appendChild(tr1)
+
+        const tr2 = document.createElement('tr')
+        const IDcontent = document.createElement('td')
+        IDcontent.innerText = BooksList[i].bookID
+        const authorContent = document.createElement('td')
+        authorContent.innerText = BooksList[i].author
+        const yearContent = document.createElement('td')
+	    yearContent.innerText = BooksList[i].year
+        tr2.appendChild(IDcontent)
+        tr2.appendChild(authorContent)
+        tr2.appendChild(yearContent)
+        tbody.appendChild(tr2)
+        table.appendChild(tbody)
+        div.appendChild(table)
+
+        bookTable.appendChild(div)
+	}
+}
---
+++ BooklistMainPage/BooklistMainPage_admin_after.js
@@ -0,0 +1,317 @@
+// global variables
+var BooklistsNum = 0; 
+var BooklistsList = [] 
+
+// temp hardcode for all books:
+var BooksNum = 0; 
+var BooksList = [] 
+class Book {
+	constructor(name, author, year, coverURL, description) {
+		this.name = name;
+		this.author = author;
+		this.year = year;
+		this.coverURL = coverURL; 
+        this.description = description;
+        this.postCollection = [] // collection of post ids associated with the book
+		this.bookID = BooksNum;
+		BooksNum++;
+	}
+}
+BooksList.push(new Book('Solaris', 'Stanisław Herman Lem', 1970, 
+'https://upload.wikimedia.org/wikipedia/en/d/d1/SolarisNovel.jpg', 
+'It follows a crew of scientists on a research station as they attempt to understand an extraterrestrial intelligence, which takes the form of a vast ocean on the titular alien planet.'))
+
+BooksList.push(new Book('Tres Tristes Tigres', 'Guillermo Cabrera Infante', 1971,
+'https://upload.wikimedia.org/wikipedia/en/0/0f/Tres_tristes_tigres_%28Guillermo_Cabrera_Infante%29.png',
+'It is a highly experimental, Joycean novel, playful and rich in literary allusions.'))
+// temp [end]
+
+class Booklist {
+	constructor(listName, listDescription, creator, bookCollection) {
+		this.listName = listName;
+		this.listDescription = listDescription;
+		this.creator = creator; // user id?
+        this.books = bookCollection; // list of bids
+		this.booklistID = BooklistsNum;
+		BooklistsNum++;
+        this.likes = 0;
+        this.collect = 0;
+        const date = new Date() 
+        this.createTime = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+	}
+}
+
+// Load default booklist data
+BooklistsList.push(new Booklist('novels', 'All novels liked.', 'A01',[BooksList[0],BooksList[1]]))
+BooklistsList.push(new Booklist('All spanish', 'All Spanish novels.', 'A01',[BooksList[1]]))
+
+
+const booklistTable = document.querySelector('#booklistTable')
+booklistTable.addEventListener('click', increaseLike)
+booklistTable.addEventListener('click', increaseCollect)
+
+// Display all availble booklists:
+function displayAllBooklists(BooklistsList) {
+    const booklistTable = document.querySelector('#booklistTable')
+	for(let i = 0; i < BooklistsNum; i++) {
+        const div = document.createElement('div')
+        div.className = 'booklist'
+
+        // <p>  list id
+        const id = document.createElement('p')
+        id.className = "listID"
+        id.appendChild(document.createTextNode("List ID: "))
+        const IDcontent = document.createElement('span')
+        IDcontent.appendChild(document.createTextNode(BooklistsList[i].booklistID))
+        id.appendChild(IDcontent)
+
+        // admin only: delete booklist
+        const button3 = document.createElement('button')
+        button3.className = "deleteButton" 
+        const deleteImg = document.createElement('img')
+        deleteImg.className = "deleteIcon"
+        deleteImg.src = "https://icon-library.com/images/icon-delete/icon-delete-4.jpg"
+        button3.appendChild(deleteImg)
+        button3.appendChild(document.createTextNode("Delete this list"))
+        id.appendChild(button3)
+
+        div.appendChild(id)
+
+        // infoWrap
+        const ul1 = document.createElement('ul')
+        ul1.className = "infoWrap"
+
+        // li1: booklist name
+        const li1 = document.createElement('li')
+        li1.className = "listname"
+        const strong1 = document.createElement('strong')
+        const name = document.createTextNode("Booklist Name: ")
+        strong1.appendChild(name)
+        const span1 = document.createElement('span')
+        const a1 = document.createElement('a')
+        a1.className = "linkColor"
+        a1.href = ""
+        const nameContent = document.createTextNode(BooklistsList[i].listName)
+        a1.appendChild(nameContent)
+        span1.appendChild(a1)
+        li1.appendChild(strong1)
+        li1.appendChild(span1)
+        ul1.appendChild(li1)
+
+        // li2: list creator
+        const li2 = document.createElement('li')
+        li2.className = "listCreator"
+        const strong2 = document.createElement('strong')
+        const creator = document.createTextNode("Created by: ")
+        strong2.appendChild(creator)
+        const span2 = document.createElement('span')
+        const a2 = document.createElement('a')
+        a2.className = "linkColor"
+        a2.href = ""
+        const creatorContent = document.createTextNode(BooklistsList[i].creator)
+        a2.appendChild(creatorContent)
+        span2.appendChild(a2)
+        li2.appendChild(strong2)
+        li2.appendChild(span2)
+        ul1.appendChild(li2)
+
+        // li3: creat time
+        const li3 = document.createElement('li')
+        li3.className = "createTime"
+        const strong3 = document.createElement('strong')
+        const time = document.createTextNode("Created when: ")
+        strong3.appendChild(time)
+        const span3 = document.createElement('span')
+        const a3 = document.createElement('a')
+        a3.className = "timeContent"
+        a3.href = ""
+        const timeContent = document.createTextNode(BooklistsList[i].createTime)
+        a3.appendChild(timeContent)
+        span3.appendChild(a3)
+        li3.appendChild(strong3)
+        li3.appendChild(span3)
+        ul1.appendChild(li3)
+
+        div.appendChild(ul1)
+
+        // list description
+        const p = document.createElement('p')
+        p.className = "descriptionsBox"
+        const strong4 = document.createElement('strong')
+        const descri = document.createTextNode("List Description: ")
+        strong4.appendChild(descri)
+        p.appendChild(strong4)
+        const span4 = document.createElement('span')
+        const descriContent = document.createTextNode(BooklistsList[i].listDescription)
+        span4.appendChild(descriContent)
+        p.appendChild(span4)
+        div.appendChild(p)
+
+        // bookshelf
+        const table = document.createElement('table')
+        table.className = "bookshelf"
+        const tbody = document.createElement('tbody')
+        const tr1 = document.createElement('tr')
+        const tr2 = document.createElement('tr')
+
+        if (BooklistsList[i].books.length <= 4){
+            bookNum = BooklistsList[i].books.length
+        } else {
+            bookNum = 4
+        }
+
+        for (let j = 0; j < bookNum; j++){
+            const newImg = document.createElement('th')
+            const img = document.createElement('img')
+            img.className = "bookimg"
+            img.src = BooklistsList[i].books[j].coverURL
+            newImg.appendChild(img)
+            tr1.appendChild(newImg)
+            const newBookLink = document.createElement('th')
+            const bookLink = document.createElement('a')
+            bookLink.className = "booklink"
+            bookLink.href = ""
+            bookLink.appendChild(document.createTextNode(BooklistsList[i].books[j].name))
+            newBookLink.appendChild(bookLink)
+            tr2.appendChild(newBookLink)
+        }
+        tbody.appendChild(tr1)
+        tbody.appendChild(tr2)
+        table.appendChild(tbody)
+        div.appendChild(table)
+
+        // icon wrap
+        const ul2 = document.createElement('ul')
+        ul2.className = "iconWrap"
+
+        // li1: like
+        const liLike = document.createElement('li')
+        liLike.className = "infoElement"
+        const button1 = document.createElement('button')
+        button1.className = "likeButton"
+        const iconImgLike = document.createElement('img')
+        iconImgLike.className = "likeIcon"
+        iconImgLike.src = "../static/like_icon.png"
+        button1.appendChild(iconImgLike)
+        liLike.appendChild(button1)
+
+        const spanLike = document.createElement('span')
+        spanLike.className = "likeNum"
+        const likeNum = document.createTextNode("Liked: "+BooklistsList[i].likes)
+        spanLike.appendChild(likeNum)
+        liLike.appendChild(spanLike)
+
+        // li2: collect
+        const liCollect = document.createElement('li')
+        liCollect.className = "infoElement"
+        const button2 = document.createElement('button')
+        button2.className = "collectButton" 
+        const iconImgCollect = document.createElement('img')
+        iconImgCollect.className = "collectIcon"
+        iconImgCollect.src = "../static/click-&-collect.png"
+        button2.appendChild(iconImgCollect)
+        liCollect.appendChild(button2)
+
+        const spanCollect = document.createElement('span')
+        spanCollect.className = "collectNum"
+        const collectNum = document.createTextNode("Collected: " + BooklistsList[i].collect)
+        spanCollect.appendChild(collectNum)
+        liCollect.appendChild(spanCollect)
+        
+        ul2.appendChild(liLike)
+        ul2.appendChild(liCollect)
+        div.appendChild(ul2)
+        
+        booklistTable.appendChild(div)
+    }
+}
+
+function increaseCollect(e){
+    e.preventDefault();
+    if (e.target.className == 'collectIcon') {
+        const index = parseInt(e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].innerText)
+        for (let i = 0; i < BooklistsNum; i++){
+            if (BooklistsList[i].booklistID == index){
+                BooklistsList[i].collect++
+                booklistTable.children[i+3].children[4].children[1].children[1].innerText = "Collected: " + BooklistsList[i].collect
+            }
+        }
+    }
+    
+}
+
+function increaseLike(e){
+    e.preventDefault();
+    if (e.target.className == 'likeIcon') {
+        const index = parseInt(e.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0].innerText)
+        for (let i = 0; i < BooklistsNum; i++){
+            if (BooklistsList[i].booklistID == index){
+                BooklistsList[i].likes++
+                booklistTable.children[i+3].children[4].children[0].children[1].innerText = "Liked: " + BooklistsList[i].likes
+            }
+        }
+    }
+    
+}
+
+// select the list sorting way
+const sort_default = document.querySelector('#sort_default')
+const sort_a_z = document.querySelector('#sort_a_z')
+sort_default.addEventListener("click",sortDefault)
+sort_a_z.addEventListener("click", sortByAtoZ)
+
+
+function sortDefault() {
+    const allBooklists = document.querySelectorAll('.booklist')
+    const length = allBooklists.length
+    for (let i = 0; i<length; i++){
+        booklistTable.removeChild(allBooklists[i])
+    }
+    return displayAllBooklists(BooklistsList)
+  }
+
+function sortByAtoZ(){
+    let nameArr = []
+    let sortedBooklistsList = []
+    for (let i=0; i<BooklistsNum; i++){
+        nameArr.push(BooklistsList[i].listName)
+    }
+    nameArr = nameArr.sort()
+    for (let i=0; i<BooklistsNum; i++) {
+        for (let j=0; j<BooklistsNum; j++){
+            if (nameArr[i] == BooklistsList[j].listName) {
+                sortedBooklistsList.push(BooklistsList[j])
+            }
+        }
+    }
+    const allBooklists = document.querySelectorAll('.booklist')
+    const length = allBooklists.length
+    for (let i = 0; i<length; i++){
+        booklistTable.removeChild(allBooklists[i])
+    }
+    return displayAllBooklists(sortedBooklistsList)
+}
+
+
+// admin only: delete list
+booklistTable.addEventListener('click', deleteBooklist)
+
+function deleteBooklist(e){
+    e.preventDefault();
+    if (e.target.className == 'deleteButton'){
+        const listElement = e.target.parentElement.parentElement
+        const booktable = listElement.parentElement
+        booktable.removeChild(listElement)
+        const ID = parseInt(listElement.children[0].children[0].innerText)
+        for (let i=0; i<BooklistsNum; i++){
+            if (BooklistsList[i].booklistID == ID){
+                console.log(ID)
+                BooklistsList.splice(i, 1)
+                BooklistsNum--
+            }
+        }
+    }
+}
+
+// load main list
+window.addEventListener("load", displayAllBooklists(BooklistsList))
