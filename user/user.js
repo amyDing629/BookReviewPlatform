d8dc30304e69f41df37918e4c578c07001034506
Yiyun Ding <yiyun.ding@mail.utoronto.ca>
2022-03-05 17:19:28 -0500

    add and collect booklist functionalities

--- user/user.js
+++ user/user.js
@@ -7,6 +7,7 @@ var BooklistsNum = 0;
 var BooksNum = 0; 
 const users = [];
 const posts = [];
+const booklists = [];
 
 /********************** Object ***********************/
 class User {
@@ -359,6 +360,58 @@ function displayUserPosts(user) {
 }
 
 function _createUserBooklists(booklist) {
+    function likeOnClick(e){
+        e.preventDefault(); // prevent default action
+        let booklistDiv = e.target.parentElement.parentElement.parentElement.parentElement;
+        let booklistID = booklistDiv.getElementsByClassName('listID')[0].children[0].innerHTML;
+        let booklist;
+        let likeNum = e.target.parentElement.parentElement.getElementsByClassName('likeNum')[0];
+        for (booklist of booklists){
+            if (parseInt(booklist.booklistID) == booklistID){
+                if (e.target.parentElement.classList.contains('likeButton')){
+                    booklist.likes ++;
+                    likeNum.innerText = 'Liked: '+ booklist.likes;
+                    e.target.parentElement.classList.remove('likeButton');
+                    e.target.parentElement.classList.add('dislikeButton');
+                    break;
+                }
+                else if (e.target.parentElement.classList.contains('dislikeButton')){
+                    booklist.likes --;
+                    likeNum.innerText = 'Liked: '+ booklist.likes;
+                    e.target.parentElement.classList.remove('dislikeButton');
+                    e.target.parentElement.classList.add('likeButton');
+                    break;
+                }    
+            } 
+        }
+    }
+
+    function collectOnClick(e){
+        e.preventDefault(); // prevent default action
+        let booklistDiv = e.target.parentElement.parentElement.parentElement.parentElement;
+        let booklistID = booklistDiv.getElementsByClassName('listID')[0].children[0].innerHTML;
+        let booklist;
+        let likeNum = e.target.parentElement.parentElement.getElementsByClassName('collectNum')[0];
+        for (booklist of booklists){
+            if (parseInt(booklist.booklistID) == booklistID){
+                if (e.target.parentElement.classList.contains('collectButton')){
+                    booklist.collect ++;
+                    likeNum.innerText = 'Collected: '+ booklist.collect;
+                    e.target.parentElement.classList.remove('collectButton');
+                    e.target.parentElement.classList.add('uncollectButton');
+                    break;
+                }
+                else if (e.target.parentElement.classList.contains('uncollectButton')){
+                    booklist.collect --;
+                    likeNum.innerText = 'Collected: '+ booklist.collect;
+                    e.target.parentElement.classList.remove('uncollectButton');
+                    e.target.parentElement.classList.add('collectButton');
+                    break;
+                }    
+            } 
+        }
+    }
+
     const div = document.createElement('div')
     div.className = 'booklist'
 
@@ -485,6 +538,7 @@ function _createUserBooklists(booklist) {
     liLike.className = "infoElement"
     const button1 = document.createElement('button')
     button1.className = "likeButton"
+    button1.addEventListener('click', likeOnClick);
     const iconImgLike = document.createElement('img')
     iconImgLike.className = "likeIcon"
     iconImgLike.src = "../static/like_icon.png"
@@ -502,6 +556,7 @@ function _createUserBooklists(booklist) {
     liCollect.className = "infoElement"
     const button2 = document.createElement('button')
     button2.className = "collectButton" 
+    button2.addEventListener('click', collectOnClick);
     const iconImgCollect = document.createElement('img')
     iconImgCollect.className = "collectIcon"
     iconImgCollect.src = "../static/click-&-collect.png"
@@ -730,6 +785,8 @@ let novelBooklist = new Booklist('novels', 'All novels liked.', 'A01', [bookSola
 let spanishBooklist = new Booklist('All spanish', 'All Spanish novels.', 'A01', [bookTres])
 regularUser.booklistList.push(novelBooklist);
 adminUser.booklistList.push(spanishBooklist);
+booklists.push(novelBooklist);
+booklists.push(spanishBooklist);
 
 regularUser.collectionList.push(postSolarisWithoutImg);
 regularUser.collectionList.push(spanishBooklist);
