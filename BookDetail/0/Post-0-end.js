22d12b72dbfd7e82fa455c7d7af812d607b5cd57
tangsiwei0831 <sw.tang@mail.utoronto.ca>
2022-03-26 19:10:21 -0400

    confirmation of add post, fix add post bug

--- BookDetail/0/Post-0-end.js
+++ BookDetail/0/Post-0-end.js
@@ -1,19 +1,20 @@
 function addNewPost(e){
     e.preventDefault();
     if (e.target.classList.contains('addSubmit,')){
-        log(2)
+        // log(2)
         const postContent = document.getElementById('postContent').value
-        log(postContent)
         const x = document.getElementById('myFile')
         const img = document.createElement("img");
-        img.src = URL.createObjectURL(x.files[0]);
-        img.className = 'postContentPicture'
+        if(x.files.length > 0){
+            img.src = URL.createObjectURL(x.files[0]);
+            img.className = 'postContentPicture'
+        }
 
         const today = new Date();
         const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours()+':'+today.getMinutes();
 
-        if(x){
-            log(3)
+        if(x.files.length > 0){
+            // log(3)
             posts.push(new Post(posts.length, 0, 'Solaris', null, 'user',
             'https://avatars.githubusercontent.com/u/71192401?v=4', img, postContent, date, 0))
         }else{
@@ -22,7 +23,13 @@ function addNewPost(e){
         }
         displayPosts();
         const postContentInput = document.getElementById('postContent')
-        postContentInput.value = ''
-        x.value = ''
+        postContentInput.value = 'add successfully!'
+        postContentInput.style = 'color: red'
+        document.getElementById('myFile').value = null
+        document.getElementById('demo').innerHTML = ''
+        setTimeout(()=>{
+            postContentInput.value = '';
+            postContentInput.style = 'color: black'
+        }, 3 * 1000)
     }
 }
--- BookDetail/3/Post-3-end.js
+++ BookDetail/3/Post-3-end.js
@@ -6,13 +6,15 @@ function addNewPost(e){
         log(postContent)
         const x = document.getElementById('myFile')
         const img = document.createElement("img");
-        img.src = URL.createObjectURL(x.files[0]);
-        img.className = 'postContentPicture'
+        if(x.files.length > 0){
+            img.src = URL.createObjectURL(x.files[0]);
+            img.className = 'postContentPicture'
+        }
 
         const today = new Date();
         const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours()+':'+today.getMinutes();
 
-        if(x){
+        if(x.files.length > 0){
             log(3)
             posts.push(new Post(posts.length, 3, 'War and Peace', null, 'user',
             'https://avatars.githubusercontent.com/u/71192401?v=4', img, postContent, date, 0))
@@ -22,7 +24,13 @@ function addNewPost(e){
         }
         displayPosts();
         const postContentInput = document.getElementById('postContent')
-        postContentInput.value = ''
-        x.value = ''
+        postContentInput.value = 'add successfully!'
+        postContentInput.style = 'color: red'
+        document.getElementById('myFile').value = null
+        document.getElementById('demo').innerHTML = ''
+        setTimeout(()=>{
+            postContentInput.value = '';
+            postContentInput.style = 'color: black'
+        }, 3 * 1000)
     }
 }
