8046b7b87d549b528181ed3ae1657b117d77f448
Yuewei Wang <yuewei.wang@mail.utoronto.ca>
2022-04-05 19:53:07 -0400

    book cover

--- public/js/BookMainPage.js
+++ public/js/BookMainPage.js
@@ -61,7 +61,7 @@ function displayAllBooks(BooksList, userID) {
             if(userType === 'Admin'){ // admin has additonal operation allowed
                 document.querySelector('#tableResult').addEventListener('click', deleteBook)
                 document.querySelector('#addButton').addEventListener('click', addNewBook)
-                document.getElementById('coverButton').addEventListener('change', _uploadPicture)
+                //document.getElementById('coverButton').addEventListener('change', _uploadPicture)
             } else {
                 document.querySelector('#adminActionsWrap').parentElement.removeChild(document.querySelector('#adminActionsWrap'))
             }
@@ -258,9 +258,9 @@ function addNewBook(e){
         const year = parseInt(document.getElementById('publishYearInput').value)
         const description = document.getElementById('descriptionInput').value
         // cover is not required
-        try {cover=URL.createObjectURL(document.getElementById("coverInput").files[0])
-        } catch {
-            cover = 'https://www.freeiconspng.com/uploads/violet-book-icon--somebooks-icons--softiconsm-11.png'
+        let cover = 'https://www.freeiconspng.com/uploads/violet-book-icon--somebooks-icons--softiconsm-11.png'
+        if (document.getElementById('coverInput').value.length > 0){
+            cover = document.getElementById('coverInput').value
         }
 
         //check validation
@@ -441,41 +441,6 @@ function _getUserID(){
         return 'guest'
     }
 }
-
-// upload img
-function _uploadPicture(e){
-    e.preventDefault
-    if (e.target.id == 'coverInput'){
-        let x = document.getElementById('coverInput')
-        let txt = "";
-        if ('files' in x) {
-          if (x.files.length == 0) {
-            txt = "Select one or more files.";
-          } else {
-            for (let i = 0; i < x.files.length; i++) {
-              txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-              let file = x.files[i];
-              if ('name' in file) {
-                txt += "name: " + file.name + "<br>";
-              }
-              if ('size' in file) {
-                txt += "size: " + file.size + " bytes <br>";
-              }
-            }
-          }
-        } 
-        else {
-          if (x.value == "") {
-            txt += "Select one or more files.";
-          } else {
-            txt += "The files property is not supported by your browser!";
-            txt  += "<br>The path of the selected file: " + x.value;
-          }
-        }
-        document.getElementById("coverUploaded").innerHTML = txt;
-    }
-}
-
 
 /*********************************** on load function *************************************/
 getBooks()
